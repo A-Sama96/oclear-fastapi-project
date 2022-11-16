cbd37d2827fb852716be1bd71f85b4c8cbe991f7
A-Sama96 <78361570+A-Sama96@users.noreply.github.com>
2022-11-16 12:50:11 +0000

    replace place correct file

--- app/place_correct.py
+++ app/place_correct.py
@@ -1,17 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
 import numpy as np
 import Levenshtein as lev
 
-def levCalclulate(str1, str2):
-    Distance = lev.distance(str1, str2)
-    Ratio = lev.ratio(str1, str2)
-    return Distance
+
+# In[3]:
 
 
 def answers(word):
     place=['DK','DL','FK','KA',"KL",'KE',"KD",'LG','SL',"SE","TC","TH",'ZG',"ZIG",'DKR',"MBR"
     ,"DAKAR","ZIGUINCHOR",'TOUBA',"MBOUR","FATICK","THIES","LOUGA","DIOUBEL",'KEDOUGOU',"KOLDA","TAMBACOUNDA","STLOUIS", "SAINTLOUIS",'KAOLACK','JOAL','GOREE','MATAM',"SEDHIOU","SALY"]
     dic=dict(enumerate(place))
-    l=[levCalclulate(word,ch) for ch in place]
-    if min(l)>4:
+    l=[lev.distance(word,ch) for ch in place]
+    a=[i for i in range(len(l)) if l[i]==min(l)]
+    p=dic[np.argmin(l)]
+    if min(l)>=3:
         return 'Pas identifie'
-    return dic[np.argmin(l)]
+    elif abs(len(word)-len(p))>2 and len(a)==1:
+        return 'Pas identifie'
+    elif len(a)!=1:
+        new=[place[j] for j in a]
+        v=[abs(len(word)-len(k)) for k in new ]
+        if min(v)<=1:
+            return new[np.argmin(v)]
+        else:
+            return 'Pas identifie'
+    return p
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
